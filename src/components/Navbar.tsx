bc3685747d0d078b03fc16aaf912ab24e6876245
森淼 <wincatcher@qq.com>
2024-10-30 20:58:50 +0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import ModeToggle from "./ModeToggle";
 export default function Navbar() {
   return (
     <nav className="flex items-center bg-base-100 dark:bg-base-300 dark:border-b dark:border-b-gray-700 justify-between p-4 shadow">
-      <Link to='/' className="text-success">在线打字练习</Link>
+      <Link to='/' className="text-success">在线英文打字练习</Link>
       <div className="flex items-center gap-2">
         <ModeToggle />
       </div>
