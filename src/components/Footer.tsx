38e05838cd1fe7def28324543fcdd8460d07c3e8
森淼 <wincatcher@qq.com>
2024-10-31 21:43:15 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,37 +3,10 @@ import { TypeIcon } from "lucide-react"
 export default function Footer() {
     return (
         <>
-            <footer className="footer p-10 bg-base-200 dark:bg-base-300 text-base-content">
-                <aside>
-                    <TypeIcon />
-                    <p>Typing Practice App.<br />Practice typing</p>
-                </aside>
-                <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </nav>
-            </footer>
-
             <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-                <aside>
-                    <p>Copyright &copy; {new Date().getFullYear()} - All right reserved by Typing Practice App</p>
-                </aside>
+            <p className="text-sm text-center">
+            Copyright © {new Date().getFullYear()} - All right reserved by <a href="https://bento.me/wincatcher" className="link link-hover text-blue-500" target="_blank" rel="noopener noreferrer">风巢森淼</a>
+          </p>
             </footer>
         </>
     )
