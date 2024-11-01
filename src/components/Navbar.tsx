4167033bdcd15adac432dbfd55a096a657a48d1f
森淼 <wincatcher@qq.com>
2024-11-01 08:56:08 +0800

    更换 icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,14 @@ import { Link } from "@tanstack/react-router";
 import ModeToggle from "./ModeToggle";
 import { GithubIcon } from "lucide-react";
 
+
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 backdrop-blur-md bg-base-100/80 dark:bg-base-300/80 border-b border-b-gray-200 dark:border-b-gray-700 flex items-center justify-between p-4 shadow">
-      <Link to='/' className="text-success">在线英文打字练习</Link>
+      <Link to='/' className="text-success flex items-center gap-2">
+        <img src="/favicon-32x32.png" alt="打字练习" className="w-8 h-8" />
+        在线英文打字练习
+      </Link>
       <div className="flex items-center gap-2">
         <ModeToggle />
         <a 
--- vite.config.ts
+++ vite.config.ts
@@ -10,30 +10,30 @@ export default defineConfig({
     TanStackRouterVite(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
         name: "Typing Practice App",
         short_name: "Typing Practice App",
         theme_color: "#ffffff",
         icons: [
           {
-            src: "pwa-64x64.png",
-            sizes: "64x64",
+            src: "favicon-16x16.png",
+            sizes: "16x16",
             type: "image/png",
           },
           {
-            src: "pwa-192x192.png",
+            src: "favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "maskable-icon-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
