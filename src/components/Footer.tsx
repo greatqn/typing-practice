6c1258a59074c9c6a734b89bfd797bed1d0ec721
森淼 <wincatcher@qq.com>
2024-11-01 11:04:25 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
     <footer className="footer p-4 bg-base-300 text-base-content mt-auto">
       <div className="container mx-auto flex flex-col items-center gap-6">
         <div className="flex flex-wrap justify-center gap-4 text-sm">
-          <Link to="/about-us/" className="link link-hover">
+          <Link to="/about-us" className="link link-hover">
             关于我们
           </Link>
-          <Link to="/terms-and-conditions/" className="link link-hover">
+          <Link to="/terms-and-conditions" className="link link-hover">
             使用条款
           </Link>
-          <Link to="/privacy/" className="link link-hover">
+          <Link to="/privacy" className="link link-hover">
             隐私政策
           </Link>
         </div>
